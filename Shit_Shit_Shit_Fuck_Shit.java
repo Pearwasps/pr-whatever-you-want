5d1aed1cabe51824d0f62c2f6b4f01d4f040881e
Reeses-Puffs <73549793+Reeses-Puffs@users.noreply.github.com>
2021-10-13 17:46:48 -0400

    Here you go
    This is the file for the GUI loop. I hope you enjoy it.

---
+++ Shit_Shit_Shit_Fuck_Shit.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Shit_Shit_Shit_Fuck_Shit {
+	
+	
+    public static void main (String args[]) {
+       
+    	while (true) { //why
+    		JFrame death = new JFrame("haha yes");
+    	    death.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	    death.setSize(300,300);
+    	    JButton weewoo = new JButton("Click to die instantly");
+    	    death.getContentPane().add(weewoo);
+    	    death.setVisible(true);
+    	}
+    	//don't do this
+    }
+}
