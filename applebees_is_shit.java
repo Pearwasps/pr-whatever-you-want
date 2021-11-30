85d8ab3a94dfc63a826197cfe0142eed4496745e
Pearwasps <95304076+Pearwasps@users.noreply.github.com>
2021-11-30 15:59:24 -0500

    the things are here
    lol

--- applebees_is_shit.java
+++ applebees_is_shit.java
@@ -1,1 +1,10 @@
-/*placeholder*/
+//I didn't test this XD
+
+public class applebees_is_shit {
+  void shit(){
+    System.out.println("Applebees is shit");
+  }
+  public static void main(String[] Pearwasps){
+    shit();
+  }
+}
